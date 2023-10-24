403a67c047c03c77985099af2edb9ae6f73cbef2
SeolAh Mo <skyloon95@gmail.com>
2023-10-24 16:19:41 +0900

    use url for get request query.

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,34 @@
 import * as http from 'http';
 import * as fs from 'fs';
+import * as url from 'url';
 
 const app = http.createServer(function (request,response){
-    let url = request.url;
+    if(request.url != null){
+        const urlData = url.parse(request.url,true);
+        const queryData = urlData.query;
+        let reqUrl = urlData.pathname;
+        let responsePage = null;
 
-    if(url == '/'){
-        url='/html/index.html';
+        console.log('reqUrl = ',reqUrl);
+        console.log('queryData = ',queryData);
+
+        if(reqUrl == '/') {
+            reqUrl = '/html/index.html';
+        }
+
+        try{
+            responsePage = fs.readFileSync(__dirname + reqUrl);
+        } catch (e){
+            console.log(e);
+            response.writeHead(404);
+            return response.end('HTTP 404 NOT FOUND ERR!');
+        }
+
+        response.writeHead(200);
+        response.end(responsePage);
+    } else {
+        response.writeHead(404);
+        return response.end('HTTP 404 NOT FOUND ERR!');
     }
-    if(request.url == '/favicon.ico'){
-        return response.writeHead(404);
-    }
-    response.writeHead(200);
-    console.log('__dirname+url = ',__dirname+url);
-    response.end(fs.readFileSync(__dirname + url));
 });
 app.listen(80);
